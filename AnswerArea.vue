2030578f95d7ee806d6c7658630acd4837cc82d3
Ping Charoenwet <ping.charoenwet@unimelb.edu.au>
2025-06-20 16:18:03 +0700

    internal utils

--- AnswerArea.vue
+++ AnswerArea.vue
@@ -55,7 +55,7 @@ import ConnectorArea from "./ConnectorArea.vue";
 import uniqueId from "lodash.uniqueid";
 import { computed } from "vue";
 import stringify from "json-stringify-pretty-compact";
-import { globalConsoleLog } from '@/utils';
+import { globalConsoleLog } from './utils';
 
 export default {
   name: "AnswerArea",
